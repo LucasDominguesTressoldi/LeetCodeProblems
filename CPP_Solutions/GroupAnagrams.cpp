6c2128532550901a6347041c906d848ad8b9688c
LucasT <tressoldi.lucas@gmail.com>
2023-12-22 13:16:40 -0300

    Group Anagrams

--- CPP_Solutions/GroupAnagrams.cpp
+++ CPP_Solutions/GroupAnagrams.cpp
@@ -4,19 +4,23 @@ public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
         unordered_map<string, vector<string>> mp;
+        vector<vector<string>> result;
 
-        for (auto x : strs)
+        for (const string &s : strs)
         {
-            string word = x;
-            sort(word.begin(), word.end());
-            mp[word].push_back(x);
+            vector<int> count(26, 0);
+
+            for (char c : s)
+                count[c - 'a']++;
+
+            string mpKey(count.begin(), count.end());
+
+            mp[mpKey].push_back(s);
         }
 
-        vector<vector<string>> ans;
-        for (auto x : mp)
-        {
-            ans.push_back(x.second);
-        }
-        return ans;
+        for (const pair<string, vector<string>> &value : mp)
+            result.push_back(value.second);
+
+        return result;
     }
 };
