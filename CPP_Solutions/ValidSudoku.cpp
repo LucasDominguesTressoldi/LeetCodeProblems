b2e12b9c5d80c99fade2c1973e6e879f3c276096
LucasT <tressoldi.lucas@gmail.com>
2024-01-11 09:58:34 -0300

    Valid Sudoku

--- CPP_Solutions/ValidSudoku.cpp
+++ CPP_Solutions/ValidSudoku.cpp
@@ -3,27 +3,37 @@ class Solution
 public:
   bool isValidSudoku(vector<vector<char>> &board)
   {
-    unordered_map<int, int> cols;
-    unordered_map<int, int> rows;
-    unordered_map<pair<int, int>, int> squares;
+    using Vector2D = pair<int, int>;
+    map<Vector2D, unordered_map<char, int>> squares;
+
+    unordered_map<int, unordered_map<char, int>> cols;
+    unordered_map<int, unordered_map<char, int>> rows;
 
     for (int r = 0; r < 9; ++r)
     {
       for (int c = 0; c < 9; ++c)
       {
-        if (board[r][c] == ".")
+        auto currNum = board[r][c];
+
+        if (currNum == '.')
         {
           continue;
         }
-        if (rows[r].contains(board[r][c]) || cols[c].contains(board[r][c]) || squares[make_pair((int)r / 3, (int)c / 3)].contains(board[r][c]))
+
+        pair<int, int> key = make_pair((int)r / 3, (int)c / 3);
+        if (rows[r].find(currNum) != rows[r].end() ||
+            cols[c].find(currNum) != cols[c].end() ||
+            squares[key].find(currNum) != squares[key].end())
         {
           return false;
         }
+
+        cols[c][currNum] = 1;
+        rows[r][currNum] = 1;
+        squares[key][currNum] = 1;
       }
-      cols[c].insert(board[r][c]);
-      rows[c].insert(board[r][c]);
-      squares[make_pair((int)r / 3, (int)c / 3)].insert(board[r][c]);
     }
+
     return true;
   }
 };
