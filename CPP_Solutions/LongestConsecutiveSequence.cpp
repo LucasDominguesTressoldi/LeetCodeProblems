700e8e8fc34adf417ecc3a45beb83e1afed75cca
LucasT <tressoldi.lucas@gmail.com>
2024-01-16 16:16:04 -0300

    Longest Consecutive Sequence - In Progress

---
+++ CPP_Solutions/LongestConsecutiveSequence.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+  int longestConsecutive(vector<int> &nums)
+  {
+    if (nums.empty())
+      return 0;
+
+    set<int> numSet(nums.begin(), nums.end());
+    int numsSize = nums.size();
+    int longest = 0;
+    int length;
+
+    for (const int &n : nums)
+    {
+      if (numSet.find(n - 1) == numSet.end())
+      {
+        length = 0;
+        while (numSet.find(n + length) != numSet.end())
+          ++length;
+        longest = max(length, longest);
+        if (longest >= numsSize)
+          return longest;
+      }
+    }
+    return longest;
+  }
+};
